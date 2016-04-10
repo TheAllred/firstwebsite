b6fac103a9c7f01c1f12743e7d63c2b5a65d513b
TheAllred <kevinawesomeallred@gmail.com>
2016-04-10 17:02:17 -0600

    Cool Squares

---
+++ scripts/squares.js
@@ -0,0 +1,25 @@
+var size = 50
+var canvas = $('.canvas')
+
+for(var x = 0; x < canvas.width(); x += size) {
+  for(var y = 0; y < canvas.height(); y += size) {
+    var seed = ((x+y*.5)/300)
+    var hue = 280 - (x/canvas.width()) * 120
+    $('<div>')
+      .addClass('square')
+      .css({
+        top: y + 'px',
+        left: x + 'px',
+        animationDelay: seed+'s',
+        background: 'hsl('+hue+', 78%, 56%)'
+      })
+      .appendTo(canvas)
+  }
+}
+
+$(window).resize(function(){
+  var size = window.innerWidth/100 * 1.3;
+  if (size < 18)
+    size = 18;
+  $('html').css({'font-size': size +'px'})
+}).resize()
